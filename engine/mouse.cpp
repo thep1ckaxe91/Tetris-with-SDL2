66bd1981b69ace18e84f97bdd9e97e7472dac53e
thep1ckaxe91 <98675539+thep1ckaxe91@users.noreply.github.com>
2024-03-04 00:52:56 +0700

    mouse done

---
+++ engine/mouse.cpp
@@ -0,0 +1,41 @@
+#include "mouse.hpp"
+#include "SDL2/SDL_mouse.h"
+#include "math.hpp"
+#include <vector>
+
+sdlgame::math::Vector2 sdlgame::mouse::get_pos()
+{
+    int x, y;
+    SDL_GetMouseState(&x, &y);
+    return sdlgame::math::Vector2(x, y);
+}
+std::vector<bool> sdlgame::mouse::get_pressed()
+{
+    int numButtons = 32;
+    Uint32 buttonState = SDL_GetMouseState(NULL, NULL);
+
+    std::vector<bool> buttons(numButtons);
+    for (int i = 0; i < numButtons; ++i)
+    {
+        buttons[i] = buttonState & (1 << i);
+    }
+    return buttons;
+}
+sdlgame::math::Vector2 sdlgame::mouse::get_rel()
+{
+    if (last_mouse_pos == sdlgame::math::Vector2(-1, -1))
+        return sdlgame::math::Vector2(0, 0);
+    int x, y;
+    SDL_GetMouseState(&x, &y);
+    sdlgame::math::Vector2 res = sdlgame::math::Vector2(x, y) - last_mouse_pos;
+    last_mouse_pos = sdlgame::math::Vector2(x, y);
+    return res;
+}
+void sdlgame::mouse::set_visible(int enable)
+{
+    isVisible = SDL_ShowCursor(enable);
+}
+bool sdlgame::mouse::get_visible()
+{
+    return isVisible;
+}
--- engine/mouse.hpp
+++ engine/mouse.hpp
@@ -1,6 +1,7 @@
 #ifndef SDLGAME_MOUSE_
 #define SDLGAME_MOUSE_
-
+#include <vector>
+#include "math.hpp"
 namespace sdlgame{
     namespace mouse
     {
@@ -8,44 +9,15 @@ namespace sdlgame{
         // best for not mess up these var some how
         namespace
         {
-            sdlgame::math::Vector2 last_mouse_pos = sdlgame::math::Vector2(-1, -1);
-            bool isVisible = true;
+            sdlgame::math::Vector2 last_mouse_pos;
+            bool isVisible;
         }
-        sdlgame::math::Vector2 get_pos()
-        {
-            int x, y;
-            SDL_GetMouseState(&x, &y);
-            return sdlgame::math::Vector2(x, y);
-        }
-        std::vector<bool> get_pressed()
-        {
-            int numButtons = 32;
-            Uint32 buttonState = SDL_GetMouseState(NULL, NULL);
-`
-            std::vector<bool> buttons(numButtons);
-            for (int i = 0; i < numButtons; ++i)
-            {
-                buttons[i] = buttonState & (1 << i);
-            }
-            return buttons;
-        }
-        sdlgame::math::Vector2 get_rel()
-        {
-            if (last_mouse_pos == sdlgame::math::Vector2(-1, -1))
-                return sdlgame::math::Vector2(0, 0);
-            int x, y;
-            SDL_GetMouseState(&x, &y);
-            sdlgame::math::Vector2 res = sdlgame::math::Vector2(x, y) - last_mouse_pos;
-            last_mouse_pos = sdlgame::math::Vector2(x, y);
-            return res;
-        }
-        void set_visible(int enable)
-        {
-            isVisible = SDL_ShowCursor(enable);
-        }
-        bool get_visible()
-        {
-            return isVisible;
-        }
+        sdlgame::math::Vector2 get_pos();
+        std::vector<bool> get_pressed();
+        sdlgame::math::Vector2 get_rel();
+        void set_visible(int enable);
+        bool get_visible();
     }
 }
+
+#endif
