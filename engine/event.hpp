24b845fe11677a15a9adf43c4a337f60de0a4f25
thep1ckaxe91 <98675539+thep1ckaxe91@users.noreply.github.com>
2024-03-04 00:14:20 +0700

    draw adn event done

--- engine/event.hpp
+++ engine/event.hpp
@@ -1,7 +1,8 @@
 #ifndef SDLGAME_EVENT_
 #define SDLGAME_EVENT_
 #include "SDL2/SDL_events.h"
-
+#include <string>
+#include <map>
 namespace sdlgame{
     typedef enum
     {
@@ -49,84 +50,15 @@ namespace sdlgame{
             Uint32 type;
             Uint32 timestamp;
             SDL_Event tmp_e;
-            Event() {}
-            Event(SDL_Event e)
-            {
-                tmp_e = e;
-                type = e.type;
-                /**
-                 * @todo: leave these here in case need, now type only is good enough
-                 */
-                if (e.type == SDL_WINDOWEVENT)
-                {
-                    timestamp = e.window.timestamp;
-                    dict["windowID"] = e.window.windowID; /**< The associated window */
-                    dict["event"] = e.window.event;
-                }
-                else if (e.type == SDL_KEYDOWN or e.type == SDL_KEYUP)
-                {
-                    timestamp = e.key.timestamp;
-                    dict["key"] = e.key.keysym.scancode;
-                }
-                else if (e.type == SDL_QUIT)
-                {
-                    // lol, just here to write const event
-                }
-                else if (e.type == SDL_MOUSEWHEEL)
-                {
-                    timestamp = e.wheel.timestamp;
-                    dict["x"] = e.wheel.x;
-                    dict["y"] = e.wheel.y;
-                }
-                else if (e.type == SDL_MOUSEBUTTONUP or e.type == SDL_MOUSEBUTTONDOWN)
-                {
-                    timestamp = e.button.timestamp;
-                    dict["button"] = e.button.button;
-                    dict["x"] = e.button.x;
-                    dict["y"] = e.button.y;
-                }
-                else if (e.type == SDL_MOUSEMOTION)
-                {
-                    timestamp = e.motion.timestamp;
-                    dict["x"] = e.motion.x;
-                    dict["y"] = e.motion.y;
-                    dict["xrel"] = e.motion.xrel;
-                    dict["yrel"] = e.motion.yrel;
-                }
-                // else if(e.type == SDL_TEXTINPUT)
-                // {
-
-                // }
-            }
-            Uint32 operator[](std::string key)
-            {
-                if (dict.find(key) != dict.end())
-                {
-                    return dict[key];
-                }
-                else
-                {
-                    throw std::invalid_argument("There's no such key: " + key);
-                }
-            }
+            Event();
+            Event(SDL_Event e);
+            Uint32 operator[](std::string key);
         };
         std::vector<Event> current_events;
-        std::vector<Event> &get()
-        {
-            SDL_PumpEvents();
-            current_events.clear();
-            SDL_Event e;
-            while (SDL_PollEvent(&e))
-                current_events.push_back(Event(e));
-            return current_events;
-        }
+        std::vector<Event> &get();
         Event tmp;
         /**please only use this for user event*/
-        void post(Uint32 event_type)
-        {
-            tmp.tmp_e.type = event_type;
-            SDL_PushEvent(&tmp.tmp_e);
-        }
+        void post(Uint32 event_type);
     }
 }
 
