581fc1e453c0c79a8970b4685836dda5e7316993
thep1ckaxe91 <98675539+thep1ckaxe91@users.noreply.github.com>
2024-03-22 16:48:15 +0700

    some random function to avoid score hacking

--- header/SaveData.hpp
+++ header/SaveData.hpp
@@ -4,8 +4,6 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-const int score_offset_byte = 9284;
-const int total_byte = score_offset_byte+283;
 
 int get_personal_best();
 void set_personal_best(int score);
--- src/SaveData.cpp
+++ src/SaveData.cpp
@@ -4,9 +4,31 @@
 
 int get_personal_best()
 {
-    
+    ifstream file(base_path+"data/save/config.dat",ios_base::binary);
+
 }
+//score must biger than get_personal_best()
 void set_personal_best(int score)
 {
+    ofstream file(base_path+"data/save/config.dat",ios_base::binary);
+    pair<int,int> data_range = {
+        sdlgame::random::randint(15,17),
+        sdlgame::random::randint(17,20)
+    };
+    //offset 0->3 is the first 4 byte of the score value, 
+    int offset[8];
+    map<int,bool> mp;
+    for(int i=0;i<8;i++)
+        do{
+            offset[i] = sdlgame::random::randint(
+                1,
+                1<<sdlgame::random::randint(
+                    data_range.first,
+                    data_range.second
+                )
+            );
+        }while(mp.at(offset[i])==1);
+    
 
+    
 }
