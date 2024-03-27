c2ef74b9452d3eb5344e50742941279bcb61c47c
thep1ckaxe91 <98675539+thep1ckaxe91@users.noreply.github.com>
2024-03-28 02:19:09 +0700

    probably this is good enough? just the pb method to avoid cheating

--- header/OptionElement.hpp
+++ header/OptionElement.hpp
@@ -1,13 +1,25 @@
 #ifndef OPTION_ELEMENT_HPP
 #define OPTION_ELEMENT_HPP
 #include "Slider.hpp"
+using namespace std;
+
+class SFXVolumeSlider : public Slider
+{
+    SFXVolumeSlider(float value, float length, float max_value);
+
+};
 
 class OptionElement
 {
 public:
-    std::string label;
+    string label;
     Surface label_surf;
-    S
+    Font label_font;
+    
+    OptionElement(string name, float value, float max_value);
+    void update();
+    void handle_event(Event &event);
+    void draw();
 };
 
 #endif
