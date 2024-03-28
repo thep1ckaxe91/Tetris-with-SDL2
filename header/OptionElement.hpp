0eee6a46028b1d7f2d4dff7e7396a57e7c2c1c65
thep1ckaxe91 <98675539+thep1ckaxe91@users.noreply.github.com>
2024-03-28 09:02:45 +0700

    publishing version 0.9

--- header/OptionElement.hpp
+++ header/OptionElement.hpp
@@ -1,25 +1,25 @@
 #ifndef OPTION_ELEMENT_HPP
 #define OPTION_ELEMENT_HPP
-#include "Slider.hpp"
-using namespace std;
+// #include "Slider.hpp"
+// using namespace std;
 
-class SFXVolumeSlider : public Slider
-{
-    SFXVolumeSlider(float value, float length, float max_value);
-
-};
-
-class OptionElement
-{
-public:
-    string label;
-    Surface label_surf;
-    Font label_font;
+// class SFXVolumeSlider : public Slider
+// {
+//     SFXVolumeSlider(float value, float length, float max_value);
     
-    OptionElement(string name, float value, float max_value);
-    void update();
-    void handle_event(Event &event);
-    void draw();
-};
+// };
+
+// class OptionElement
+// {
+// public:
+//     string label;
+//     Surface label_surf;
+//     Font label_font;
+    
+//     OptionElement(string name, float value, float max_value);
+//     void update();
+//     void handle_event(Event &event);
+//     void draw();
+// };
 
 #endif
