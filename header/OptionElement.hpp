deaa9b12730b63d97116bf41662ccea56a66cc66
thep1ckaxe91 <98675539+thep1ckaxe91@users.noreply.github.com>
2024-03-28 12:10:41 +0700

    fix minor stuff

--- header/OptionElement.hpp
+++ header/OptionElement.hpp
@@ -1,25 +1,28 @@
 #ifndef OPTION_ELEMENT_HPP
 #define OPTION_ELEMENT_HPP
-// #include "Slider.hpp"
-// using namespace std;
+#include "Slider.hpp"
+using namespace std;
 
-// class SFXVolumeSlider : public Slider
-// {
-//     SFXVolumeSlider(float value, float length, float max_value);
-    
-// };
+class SFXVolumeSlider : public Slider
+{
+    SFXVolumeSlider(Vector2 topleft, float value, float length, float max_value);
+    void update();
+    void handle_event();
+    void draw();
+    void on_change_value();
+};
 
-// class OptionElement
-// {
-// public:
-//     string label;
-//     Surface label_surf;
-//     Font label_font;
+class OptionElement
+{
+public:
+    string label;
+    Surface label_surf;
+    Font label_font;
     
-//     OptionElement(string name, float value, float max_value);
-//     void update();
-//     void handle_event(Event &event);
-//     void draw();
-// };
+    OptionElement(Vector2 topleft, string name, float value, float max_value);
+    void update();
+    void handle_event(Event &event);
+    void draw();
+};
 
 #endif
--- src/OptionElement.cpp
+++ src/OptionElement.cpp
@@ -1,1 +1,3 @@
 #include "OptionElement.hpp"
+
+SFXVolumeSlider::SFXVolumeSlider(Vector2 topleft, float val, float length, float max_val) : Slider()
